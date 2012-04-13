dccadfa566f52413c4d44bf2a403686f612715c4
Sahas <sahas052@gmail.com>
2012-04-14 01:23:27 +0530

    Update anagram.c

--- anagram.c
+++ anagram.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
+#include<string.h>
 
+void anagram(char* first,char* second){
+  
+  
+}
 void main(){
 
+char *first,*second;
+printf("Enter the first,second strings\n");
+scanf("%s",first);
+scanf("%s",second);
+if(strlen(first)!=strlen(second))
+printf("Not Anagrams");
+else
+anagram(first,second);
 }
