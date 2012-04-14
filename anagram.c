29ddcfbb84dba50ebee23c5b2a4464705654f380
ide user ide_sahas <sahas052@gmail.com>
2012-04-14 19:51:16 +0000

    first commit

--- anagram.c
+++ anagram.c
@@ -1,12 +1,34 @@
 #include<stdio.h>
 #include<string.h>
+enum Boolean{false=0,true=1};
+typedef enum Boolean boolean;
 
-void anagram(char* first,char* second){
-  
-sort(first);
-sort(second);
-if(strcmp())
-  
+void trim(char* str){
+    int i,j;
+    for(i=0;str[i]!='\0';i++){
+        if(str[i]==' '){
+            for(j=i;str[j]!='\0';j++)
+            str[j]=str[j+1];
+        }
+    }
+}
+
+void sort(char* str){
+    
+}
+
+boolean isAnagram(char* first,char* second){
+    trim(first);
+    trim(second);
+    if(strlen(first)!=strlen(second))
+        printf("Not Anagrams");
+    sort(first);
+    sort(second);
+    if(strcmp(first,second)){
+    return false;
+    }
+    else
+    return true;
 }
 void main(){
 
@@ -14,8 +36,8 @@ char *first,*second;
 printf("Enter the first,second strings\n");
 scanf("%s",first);
 scanf("%s",second);
-if(strlen(first)!=strlen(second))
-printf("Not Anagrams");
+if(isAnagram(first,second))
+printf("Anagrams");
 else
-anagram(first,second);
+printf("Not Anagrams");
 }
